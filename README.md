6a7de1dc5bbc415fa385115234cc94fece43b86b
Hubert Jetschko <hubert@robospace.co.nz>
2018-04-04 15:38:44 +1200

    + First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Python Tutorial
+
+This repository contains the tutorial files Python V2.7 and V3 for our library.
+
+To install our Python libraries use the following command:
+pip install webifi
+
+To upgrade to the latest version of the library:
+pip install webifi --upgrade
+
+If you prefer to download the libraries:
+https://github.com/webifi-me/WebifiPythonLibrary
+
+If you downloaded the libraries then you also need to install the websocket client. Please use the following command to install websocket client:
+pip install websocket-client
+
+The tutorial can be found at:
+https://www.webifi.me/python-tutorial/
+
+The library documentation can be found at:
+https://www.webifi.me/python-library-documentation/
---
+++ WebifiPythonTutorial_p27.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python
+
+import Webifi_p27
+import Tkinter
+import tkMessageBox
+
+class GuiControls():
+    def __init__(self):
+        gui = Tkinter.Tk()
+        pad_x = 5
+        pad_y = 3
+        self.gui = gui
+        self.webifi = None
+        self.gui_row_number = 0
+
+        gui.title('Webifi Tutorial')
+
+        # connect name, password, set network names and start button
+        Tkinter.Label(gui, text='Connect name:').grid(row=self.get_row_num(False), column=0, padx=pad_x-2, pady=pad_y, sticky='W')
+        self.entry_connect_name = Tkinter.Entry(gui, width=25)
+        self.entry_connect_name.insert(0, 'connect name')
+        self.entry_connect_name.grid(row=self.get_row_num(True), column=0, padx=pad_x, pady=0, sticky='W')
+        Tkinter.Label(gui, text='Connect password:').grid(row=self.get_row_num(True), column=0, padx=pad_x-2, pady=pad_y, sticky='W')
+        self.entry_connect_password = Tkinter.Entry(gui, width=25)
+        self.entry_connect_password.grid(row=self.get_row_num(True), column=0, padx=pad_x, pady=0, sticky='W')
+        self.entry_connect_password.insert(0, 'password')
+        Tkinter.Label(gui, text='Network names:').grid(row=self.get_row_num(True), column=0, padx=pad_x-2, pady=pad_y, sticky='W')
+        self.text_network_names = Tkinter.Text(gui, wrap=Tkinter.WORD, undo=True, height=5, width=15)
+        self.text_network_names.grid(row=self.get_row_num(True), column=0, columnspan=1, padx=pad_x-2, pady=0, sticky='WE')
+        self.scroll_network_names = Tkinter.Scrollbar(gui, command=self.text_network_names.yview)
+        self.scroll_network_names.grid(row=self.get_row_num(False), column=0, sticky='nsew')
+        self.text_network_names['yscrollcommand'] = self.scroll_network_names.set
+        self.text_network_names.insert(Tkinter.INSERT, 'network 1')
+        self.button_set_network_names = Tkinter.Button(gui, text='Set network names', width=16, command=self.click_button_set_network_names)
+        self.button_set_network_names.grid(row=self.get_row_num(True), column=0, padx=pad_x, pady=pad_y, sticky='W')
+        self.button_start = Tkinter.Button(gui, text='Start', width=12, command=self.click_button_start)
+        self.button_start.grid(row=self.get_row_num(True), column=0, padx=pad_x, pady=pad_y, sticky='W')
+
+        # send data controls
+        Tkinter.Label(gui, text='Text to send:').grid(row=self.get_row_num(True), column=0, padx=pad_x-2, pady=pad_y, sticky='W')
+        self.entry_send_data = Tkinter.Entry(gui, width=56)
+        self.entry_send_data.grid(row=self.get_row_num(True), column=0, columnspan=2, padx=pad_x, pady=pad_y, sticky='W')
+        self.entry_send_data.insert(0, 'test message')
+        self.button_send = Tkinter.Button(gui, text='Send', width=12, command=self.click_send_data)
+        self.button_send.grid(row=self.get_row_num(True), column=0, padx=pad_x, pady=pad_y, sticky='W')
+        self.button_send_discovery = Tkinter.Button(gui, text='Send discovery', width=12, command=self.click_send_discovery)
+        self.button_send_discovery.grid(row=self.get_row_num(False), column=1, padx=pad_x, pady=pad_y, sticky='W')
+
+        # received data
+        Tkinter.Label(gui, text='Text received:').grid(row=self.get_row_num(True), column=0, padx=pad_x-2, pady=pad_y, sticky='W')
+        self.text_rec_data = Tkinter.Text(gui, wrap=Tkinter.WORD, undo=True, height=5, width=40)
+        self.text_rec_data.grid(row=self.get_row_num(True), column=0, columnspan=3, padx=pad_x-2, pady=0, sticky='WE')
+        self.scroll_rec_data = Tkinter.Scrollbar(gui, command=self.text_rec_data.yview)
+        self.scroll_rec_data.grid(row=self.get_row_num(False), column=3, sticky='nsew')
+        self.text_rec_data['yscrollcommand'] = self.scroll_rec_data.set
+        self.button_clear_rec_data = Tkinter.Button(gui, text='Clear', width=12, command=self.click_clear_rec_data)
+        self.button_clear_rec_data.grid(row=self.get_row_num(True), column=0, padx=pad_x, pady=pad_y, sticky='W')
+
+        # messages
+        Tkinter.Label(gui, text='Messages:').grid(row=self.get_row_num(True), column=0, padx=pad_x-2, pady=pad_y, sticky='W')
+        self.text_messages = Tkinter.Text(gui, wrap=Tkinter.WORD, undo=True, height=5, width=40)
+        self.text_messages.grid(row=self.get_row_num(True), column=0, columnspan=3, padx=pad_x-2, pady=0, sticky='WE')
+        self.scroll_messages = Tkinter.Scrollbar(gui, command=self.text_messages.yview)
+        self.scroll_messages.grid(row=self.get_row_num(False), column=3, sticky='nsew')
+        self.text_messages['yscrollcommand'] = self.scroll_messages.set
+        self.button_clear_messages = Tkinter.Button(gui, text='Clear', width=12, command=self.click_clear_messages)
+        self.button_clear_messages.grid(row=self.get_row_num(True), column=0, padx=pad_x, pady=pad_y, sticky='W')
+
+        # set callback for when the window closes. This will terminate the Webifi service and
+        # stop all the threads
+        self.gui.protocol("WM_DELETE_WINDOW", self.form_closing)
+
+        gui.mainloop()
+
+    def get_row_num(self, new_row):
+        if new_row:
+            self.gui_row_number += 1
+        return  self.gui_row_number
+
+    def add_message(self, message):
+        self.text_messages.insert(Tkinter.END, message + '\n')
+        self.text_messages.see(Tkinter.END)
+
+    def data_received_callback(self, data, data_type, from_who):
+        if self.webifi.get_discoverable() and data_type == 'Discovery Response':
+            # valid discovery response received
+            self.add_message('Device Discovered: ' + data + ', ID: ' + str(from_who))
+        else:
+            message = data + ', ' + data_type + ', ' + str(from_who)
+            self.text_rec_data.insert(Tkinter.END, message + '\n')
+            self.text_rec_data.see(Tkinter.END)
+
+    def connection_status_callback(self, connected):
+        if connected:  # connection was successful
+            self.add_message('Connection successful')
+        else:
+            # there was an error
+            self.add_message('Connection failed')
+            self.button_start['text'] = 'Start'
+            self.webifi = None
+
+    def click_button_set_network_names(self):
+        if self.webifi is not None:
+            network_names = self.text_network_names.get("1.0", Tkinter.END).split('\n')
+            self.webifi.set_network_names(network_names)
+
+    def click_send_discovery(self):
+        if self.webifi is not None:
+            self.webifi.send_discovery()
+
+    def click_button_start(self):
+        if self.webifi is None:
+            connect_name = self.entry_connect_name.get()
+            connect_password = self.entry_connect_password.get()
+            network_names = self.text_network_names.get("1.0", Tkinter.END).split('\n')
+            input_error = False
+            if connect_name is '':
+                tkMessageBox.showerror('Connect name error', 'Please enter a valid connect name')
+                input_error = True
+            if connect_password is '':
+                tkMessageBox.showerror('Password error', 'Please enter a valid password')
+                input_error = True
+            if not input_error:
+                # start webifi service
+                self.webifi = Webifi_p27.Webifi()
+                self.webifi.set_connect_name(connect_name)
+                self.webifi.set_connect_password(connect_password)
+                self.webifi.set_network_names(network_names)
+                self.webifi.set_connection_status_callback(self.connection_status_callback)
+                self.webifi.set_data_received_callback(self.data_received_callback)
+                self.webifi.name = 'Webifi Python Tutorial'
+                self.webifi.start()
+            self.button_start['text'] = 'Stop'
+        else:
+            self.webifi.close_connection()
+            self.button_start['text'] = 'Start'
+            self.webifi = None
+
+    def click_send_data(self):
+        if self.webifi is not None:
+            send_data = Webifi_p27.CreateSendData()
+            send_data.data = self.entry_send_data.get()
+            send_data.data_type = 'data'
+            if self.webifi.connected:
+                self.webifi.send_data(send_data)
+        else:
+            tkMessageBox.showerror('Please start service', 'The service needs to be started before this action can be performed')
+
+    def click_clear_rec_data(self):
+        self.text_rec_data.delete(1.0, Tkinter.END)
+
+    def click_clear_messages(self):
+        self.text_messages.delete(1.0, Tkinter.END)
+
+    def form_closing(self):
+        if self.webifi is not None:
+            self.webifi.close_connection(True)
+        self.webifi = None
+        self.gui.destroy()
+
+
+if __name__ == "__main__":
+    gui_controls = GuiControls()
